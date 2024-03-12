8bc5514f5abb3babb4ec9de56971a78671bd896c
Thomson Ly <thomson.ly98@gmail.com>
2024-03-12 18:06:32 -0400

    First commit to track activity

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+<h1>
+    <?php
+        // Sample Comment
+        # Comment
+        $x = 10;
+        $y = 5;
+        $name = "Thomson";
+    ?>
+    <?= "Hello {$name}"?>
+</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
